ecf79fccad6ff41b8c1a414962b98c24131c7855
Brian Huffman <brianh@cs.pdx.edu>
2008-12-10 16:46:20 -0800

    optimized solution

--- Euler220.hs
+++ Euler220.hs
@@ -27,90 +27,332 @@ What is the position of the cursor after 10^(12) steps in D_(50) ?
 Give your answer in the form x,y with no spaces.
 -}
 
-a :: Int -> String
-a 0 = "a"
-a n = a (n-1) ++ "R" ++ b (n-1) ++ "FR"
 
-b :: Int -> String
-b 0 = "b"
-b n = "LF" ++ a (n-1) ++ "L" ++ b (n-1)
+{-
+a(1) = RFR
+b(1) = LFL
+a -> aRbFR
+b -> LFaLb
 
-d :: Int -> String
-d n = "F" ++ a n
+LFa -> LFaRbFR
+bFR -> LFaLbFR
 
-run :: String -> ((Int, Int), Int)
-run [] = ((0, 0), 0)
-run (c : cs) =
-  case c of
-    'F' -> ((x, y+1), n+1)
-    'L' -> ((-y, x), n)
-    'R' -> ((y, -x), n)
-    _ -> ((x, y), n)
+Let c = LFa, d = bFR
+c(1) = LFRFR  (net turn of R)
+d(1) = LFLFR  (net turn of L)
+c -> cRd
+d -> cLd
+
+D(n) contains 2^n moves.
+D(n) is a prefix of D(n+1).
+
+  1-2          2
+  | |         / \
+  0 3-4      0   4     0---4    0       0
+      |         /          |     \      |
+    6-5        6           |      \     |
+    |           \          |       \    |
+  C-7-8      C   8     C---8        8   |
+  | | |     / \ /      |           /    |
+E-D A-9    E   A       |          /     |
+|           \          |         /      |
+F-G          G         G        G       G
+
+If we have
+  dragon(n) = (x,y)
+  dragon(n+1) = (u,v)
+
+then we can calculate
+  dragon(2n) = (x+y, y-x)
+  dragon(2n+1) = (x+y, v-u)
+  dragon(2n+2) = (u+v, v-u)
+
+Alternate system in terms of absolute directions:
+  n -> ne
+  e -> se
+  s -> sw
+  w -> nw
+-}
+
+-- dragon' n = (dragon n, dragon (n+1))
+dragon' 0 = ((0, 0), (0, 1))
+dragon' n
+  | even n = ((x+y, y-x), (x+y, v-u))
+  | otherwise = ((x+y, v-u), (u+v, v-u))
   where
-    ((x, y), n) = run cs
+    ((x, y), (u, v)) = dragon' (n `div` 2)
 
-------------------------------------------
-
-type Z = Integer
-
-a_seq :: [(Z, Z)]
-a_seq = f 0 0
-  where f x y = (x, y) : f (x+y+1) (y-x)
-
-a_dist :: Int -> Integer
-a_dist n = 2^n - 1
-
-data Cmd = L | R | F | A Int | B Int
-
-data Dir = North | West | South | East
-  deriving Show
-
-type Move = (Integer, Integer, Dir)
-
-thenDir :: Dir -> Dir -> Dir
-thenDir North y = y
-thenDir x North = x
-thenDir West West = South
-thenDir West South = East
-thenDir West East = North
-thenDir South West = East
-thenDir South South = North
-thenDir South East = West
-thenDir East West = North
-thenDir East South = West
-thenDir East East = South
-
-thenMove :: Move -> Move -> Move
-thenMove (x1, y1, North) (x2, y2, d) = (x1+x2, y1+y2, d)
-thenMove (x1, y1, South) (x2, y2, d) = (x1-x2, y1-y2, thenDir South d)
-thenMove (x1, y1, East) (x2, y2, d) = (x1+y2, y1-x2, thenDir East d)
-thenMove (x1, y1, West) (x2, y2, d) = (x1-y2, y1+x2, thenDir West d)
-
-runCmd :: Cmd -> Integer -> (Move, Maybe Integer)
-runCmd L m = ((0, 0, West), Just m)
-runCmd R m = ((0, 0, East), Just m)
-runCmd F m
-  | m < 1 = ((0, 0, North), Nothing)
-  | otherwise = ((0, 1, North), Just (m-1))
-runCmd (A 0) m = ((0, 0, North), Just m)
-runCmd (A n) m
-  | m < a_dist n = runCmds [A(n-1), R, B(n-1), F, R] m
-  | otherwise = let (x, y) = a_seq!!n in ((x, y, South), Just (m - a_dist n))
-runCmd (B 0) m = ((0, 0, North), Just m)
-runCmd (B n) m
-  | m < a_dist n = runCmds [L, F, A(n-1), L, B(n-1)] m
-  | otherwise = let (x, y) = a_seq!!n in ((-x, -y, South), Just (m - a_dist n))
-
-runCmds :: [Cmd] -> Integer -> (Move, Maybe Integer)
-runCmds [] m = ((0, 0, North), Just m)
-runCmds (c : cs) m =
-  let (move1, mm') = runCmd c m in
-    case mm' of
-      Nothing -> (move1, Nothing)
-      Just m' -> let (move2, mm'') = runCmds cs m' in
-                   (move1 `thenMove` move2, mm'')
+dragon :: Integer -> (Integer, Integer)
+dragon n = fst (dragon16 n)
 
 main :: IO String
 main = return (show x ++ "," ++ show y)
+  where (x, y) = dragon (10^12)
+
+
+{-
+Representing ((x,y),(u,v)) by ((x,y),delta).
+
+    N
+    |
+ W--+--E
+    |
+    S
+
+n -> 2n, 2n+1
+((x,y), N) -> ((x+y, y-x), N), ((x+y, y-x + 1), E)
+((x,y), E) -> ((x+y, y-x), S), ((x+y, y-x - 1), E)
+((x,y), S) -> ((x+y, y-x), S), ((x+y, y-x - 1), W)
+((x,y), W) -> ((x+y, y-x), N), ((x+y, y-x + 1), W)
+
+(x,y) -> (x+y, y-x) -> (2y, -2x) -> (2y-2x, -2x-2y) -> (-4x, -4y)
+
+n -> ne -> nese -> neseswse -> neseswseswnwswse
+e -> se -> swse -> swnwswse -> swnwnenwswnwswse
+s -> sw -> swnw -> swnwnenw -> swnwnenwnesenenw
+w -> nw -> nenw -> nesenenw -> neseswsenesenenw
+-}
+
+data Delta = N | E | S | W
+  deriving (Eq, Show)
+
+next_x :: Delta -> Integer -> Integer
+next_x d x = case d of { N -> x; E -> x+1; S -> x; W -> x-1 }
+
+next_y :: Delta -> Integer -> Integer
+next_y d y = case d of { N -> y+1; E -> y; S -> y-1; W -> y }
+
+d2n :: Delta -> Delta
+d2n d = case d of { N -> N; E -> S; S -> S; W -> N }
+
+d2n1 :: Delta -> Delta
+d2n1 d = case d of { N -> E; E -> E; S -> W; W -> W }
+
+dragon'' 0 = ((0, 0), N)
+dragon'' n
+  | even n = ((x+y, y-x), d2n d)
+  | otherwise = ((x+y, next_y (d2n d) (y-x)), d2n1 d)
   where
-    ((x,y,d),_) = runCmds [F, A 50] (10^12)
+    ((x, y), d) = dragon'' (n `div` 2)
+
+-- Reducing mod 4
+dragon4 0 = ((0, 0), N)
+dragon4 n = x' `seq` y' `seq`
+  case d of
+    N -> case r of
+           0 -> ((x'+0, y'+0), N)
+           1 -> ((x'+0, y'+1), E)
+           2 -> ((x'+1, y'+1), S)
+           3 -> ((x'+1, y'+0), E)
+    E -> case r of
+           0 -> ((x'+0, y'+0), S)
+           1 -> ((x'+0, y'-1), W)
+           2 -> ((x'-1, y'-1), S)
+           3 -> ((x'-1, y'-2), E)
+    S -> case r of
+           0 -> ((x'+0, y'+0), S)
+           1 -> ((x'+0, y'-1), W)
+           2 -> ((x'-1, y'-1), N)
+           3 -> ((x'-1, y'+0), W)
+    W -> case r of
+           0 -> ((x'+0, y'+0), N)
+           1 -> ((x'+0, y'+1), E)
+           2 -> ((x'+1, y'+1), N)
+           3 -> ((x'+1, y'+2), W)
+  where
+    (n', r) = divMod n 4
+    ((x, y), d) = dragon4 n'
+    (x', y') = (2*y, -2*x)
+
+-- Reducing mod 16
+dragon16 0 = ((0, 0), N)
+dragon16 n = x' `seq` y' `seq`
+  case d of
+    N -> case r of
+           0  -> ((x'+0, y'+0), N)
+           1  -> ((x'+0, y'+1), E)
+           2  -> ((x'+1, y'+1), S)
+           3  -> ((x'+1, y'+0), E)
+           4  -> ((x'+2, y'+0), S)
+           5  -> ((x'+2, y'-1), W)
+           6  -> ((x'+1, y'-1), S)
+           7  -> ((x'+1, y'-2), E)
+           8  -> ((x'+2, y'-2), S)
+           9  -> ((x'+2, y'-3), W)
+           10 -> ((x'+1, y'-3), N)
+           11 -> ((x'+1, y'-2), W)
+           12 -> ((x'+0, y'-2), S)
+           13 -> ((x'+0, y'-3), W)
+           14 -> ((x'-1, y'-3), S)
+           15 -> ((x'-1, y'-4), E)
+    E -> case r of
+           0  -> ((x'+0, y'+0), S)
+           1  -> ((x'+0, y'-1), W)
+           2  -> ((x'-1, y'-1), N)
+           3  -> ((x'-1, y'+0), W)
+           4  -> ((x'-2, y'+0), N)
+           5  -> ((x'-2, y'+1), E)
+           6  -> ((x'-1, y'+1), N)
+           7  -> ((x'-1, y'+2), W)
+           8  -> ((x'-2, y'+2), S)
+           9  -> ((x'-2, y'+1), W)
+           10 -> ((x'-3, y'+1), N)
+           11 -> ((x'-3, y'+2), W)
+           12 -> ((x'-4, y'+2), S)
+           13 -> ((x'-4, y'+1), W)
+           14 -> ((x'-5, y'+1), S)
+           15 -> ((x'-5, y'+0), E)
+    S -> case r of
+           0  -> ((x'+0, y'+0), S)
+           1  -> ((x'+0, y'-1), W)
+           2  -> ((x'-1, y'-1), N)
+           3  -> ((x'-1, y'+0), W)
+           4  -> ((x'-2, y'+0), N)
+           5  -> ((x'-2, y'+1), E)
+           6  -> ((x'-1, y'+1), N)
+           7  -> ((x'-1, y'+2), W)
+           8  -> ((x'-2, y'+2), N)
+           9  -> ((x'-2, y'+3), E)
+           10 -> ((x'-1, y'+3), S)
+           11 -> ((x'-1, y'+2), E)
+           12 -> ((x'+0, y'+2), N)
+           13 -> ((x'+0, y'+3), E)
+           14 -> ((x'+1, y'+3), N)
+           15 -> ((x'+1, y'+4), W)
+    W -> case r of
+           0  -> ((x'+0, y'+0), N)
+           1  -> ((x'+0, y'+1), E)
+           2  -> ((x'+1, y'+1), S)
+           3  -> ((x'+1, y'+0), E)
+           4  -> ((x'+2, y'+0), S)
+           5  -> ((x'+2, y'-1), W)
+           6  -> ((x'+1, y'-1), S)
+           7  -> ((x'+1, y'-2), E)
+           8  -> ((x'+2, y'-2), N)
+           9  -> ((x'+2, y'-1), E)
+           10 -> ((x'+3, y'-1), S)
+           11 -> ((x'+3, y'-2), E)
+           12 -> ((x'+4, y'-2), N)
+           13 -> ((x'+4, y'-1), E)
+           14 -> ((x'+5, y'-1), N)
+           15 -> ((x'+5, y'+0), W)
+  where
+    (n', r) = divMod n 16
+    ((x, y), d) = dragon16 n'
+    (x', y') = (-4*x, -4*y)
+
+{-
+(x,y) -> (x+y, y-x) -> (2y, -2x) -> (2y-2x, -2x-2y) -> (-4x, -4y)
+
+n -> ne -> nese -> neseswse -> neseswseswnwswse
+e -> se -> swse -> swnwswse -> swnwnenwswnwswse
+s -> sw -> swnw -> swnwnenw -> swnwnenwnesenenw
+w -> nw -> nenw -> nesenenw -> neseswsenesenenw
+-}
+
+{-
+dragon_r n = dragon_d (2*n)
+
+let n = 2^k * m, with m odd.
+if k > 0, then dragon_d(2^k * m) = dragon_d(2m).
+
+10111010100: ((14,-12),N)
+10111010000: ((16,-12),S)
+10111000000: ((16,-8),N)
+10110000000: ((24,-8),N)
+10100000000: ((32,-16),N)
+10000000000: ((32,0),S)
+
+-----------------------------
+-----------------------------
+10111010100: r^2 (w)    (rev)
+-----------------------------
+10111010000: r^4 (s)
+-----------------------------
+10111000000: r^6 (w)
+10110000000: r^7 (nw)
+10100000000: r^8 (s)    (rev)
+-----------------------------
+10000000000: r^10 (e)
+
+10111010100
+
+
+11000111011: r^0 (s)    (rev)
+11000111010: r^1 (ne)
+11000111000: r^3 (se)
+11000110000: r^4 (s)
+11000100000: r^5 (ne)   (rev)
+11000000000: r^9 (sw)   (rev)
+10000000000: r^10 (e)
+
+11000111011
+eessswwwnee
+-----++++--
++-   -++ +-
+
+10111010100
+eswwwneswnn
+
+
+0 -> n
+1 -> ne
+2 -> e
+3 -> se
+4 -> s
+5 -> sw
+6 -> w
+7 -> nw
+8 -> n
+
+1-2
+| |
+0 3-4
+
+4-3 0
+  | |
+  2-1
+
+  0   4-3
+  |     |
+2-1   1-2
+|     |
+3-4   0
+-}
+
+dragon_ds :: [Delta]
+dragon_ds = N : E : concatMap f (tail dragon_ds)
+  where
+    f N = [N,E]
+    f E = [S,E]
+    f S = [S,W]
+    f W = [N,W]
+
+dragon_rs = f dragon_ds
+  where f (x:y:zs) = x : f zs
+
+dragon_d :: Integer -> Delta
+dragon_d 0 = N
+dragon_d n =
+  case d of
+    N -> if even n then N else E
+    E -> if even n then S else E
+    S -> if even n then S else W
+    W -> if even n then N else W
+  where d = dragon_d (n `div` 2)
+
+{-
+N S S S S N S S S N N N S N S S
+NSSSSNSSSNNNSNSSSNNNNSNNSNNNSNSS
+
+dragon_r(2n) = dragon_r(n)
+dragon_r(2n+1) = dragon_r(n) for odd n.
+dragon_r(2n+1) = -dragon_r(n) for even n.
+
+f(4n+0) = f(2n)
+f(4n+1) = f(2n) (rev)
+f(4n+2) = f(2n+2)
+f(4n+3) = f(2n+2)
+
+-}
+
