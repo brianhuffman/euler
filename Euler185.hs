0bf7b468eb7d9d4c972bf58ab65f18993a390b88
Brian Huffman <brianh@cs.pdx.edu>
2009-02-01 12:38:45 -0800

    clean up 185

--- Euler185.hs
+++ Euler185.hs
@@ -10,13 +10,15 @@ Problem 185
 
 The game Number Mind is a variant of the well known game Master Mind.
 
-Instead of coloured pegs, you have to guess a secret sequence of digits.
-After each guess you're only told in how many places you've guessed the
-correct digit. So, if the sequence was 1234 and you guessed 2036, you'd
-be told that you have one correct digit; however, you would NOT be told
-that you also have another digit in the wrong place.
+Instead of coloured pegs, you have to guess a secret sequence of
+digits. After each guess you're only told in how many places you've
+guessed the correct digit. So, if the sequence was 1234 and you
+guessed 2036, you'd be told that you have one correct digit; however,
+you would NOT be told that you also have another digit in the wrong
+place.
 
-For instance, given the following guesses for a 5-digit secret sequence,
+For instance, given the following guesses for a 5-digit secret
+sequence,
 
 90342 ;2 correct
 70794 ;0 correct
@@ -142,115 +144,6 @@ position x has
 
 -}
 
-{-
-prob185a clues = map (map bit2char . elems) $ f xs0 clues''
-  where
-    clues' = sortOf snd clues
-    clues'' = [ (zip [0 ..] (map digitToInt s), n) | (s,n) <- clues' ]
-    l = length $ fst $ head clues
-    xs0 = accumArray undefined 0x3ff (0,l-1) []
-
-    f xs [] = [xs]
-    f xs ((cs,0):clues) =
-      [ xs'' |
-        let xs' = accum clearBit xs cs,
-        all (\i -> xs'!i /= 0) (map fst cs),
-        xs'' <- f xs' clues ]
-    f xs ((cs,n):clues) =
-      [ xs'' |
-        (c'@(p,d),cs') <- remove1 cs,
-        testBit (xs!p) d,
-        let xs' = accum (\e c -> e .&. bit c) xs [c'],
-        xs'' <- f xs' ((cs',n-1):clues) ]
-
-hamming_distance :: (Eq a) => [a] -> [a] -> Int
-hamming_distance [] [] = 0
-hamming_distance (x:xs) (y:ys)
-  | x == y = hamming_distance xs ys
-  | otherwise = hamming_distance xs ys + 1
-
-prob185z :: String -> Int
-prob185z s0 = sum
-  [ abs (m - n) |
-    (s,n) <- real_clues,
-    let m = length $ filter id $ zipWith (==) s0 s ]
-
--- all strings within Hamming distance n
-prob185y 0 xs = [xs]
-prob185y n [] = [[]]
-prob185y n (x:xs) =
-  [ x':xs' |
-    x' <- "0123456789",
-    xs' <- prob185y (if x == x' then n else n-1) xs ]
-
-foldl1' f (x:xs) = rec x xs
-  where
-    rec x [] = x
-    rec x (y:ys) = (rec $! (f x y)) ys
-
-minimumOf' f = foldl1' (\x y -> if f x < f y then x else y)
-
-prob185x n s = (s, prob185z s) : prob185x (if s == s' then n+1 else 1) s'
-  where
-    s' = minimumOf' prob185z $ prob185y n s
-
-prob185d :: [Int] -> [Clue] -> [[(Int, Char)]]
-prob185d [] clues = [[]]
-prob185d xs clues =
-  [ (x,d):rs |
-    (x,d) <- xds,
-    let xs' = delete x xs,
-    let clues' = [ (s, if s!!x == d then n-1 else n) | (s,n) <- clues ],
-    rs <- prob185d xs' clues'
-  ]
-  where
-    l = length xs
-    xds = [ (x,d) | (_,x,d) <- sort choices ]
-    choices =
-      [ (-r,x,d) |
-        x <- xs,
-        d <- "0123456789",
-        let r = product [ if d == d' then 9*n else l-n |
-                          (s,n) <- clues, let d' = s!!x ],
-        r /= 0 ]
-
-prob185e l clues = map f (prob185d [0 .. l-1] clues)
-  where
-    f xs = elems $ accumArray (const id) '?' (0,l-1) xs
-
-
-prob185c :: Int -> [(String,Int)] -> [String]
-prob185c 0 clues = if all ((==0) . snd) clues then [""] else []
-prob185c l clues =
-  [ c : cs |
-    let a = accumArray (*) 1 ('0','9') [ (e,n) | (e:es, n) <- clues ],
-    let xs = reverse $ map fst $ sortOf snd $ filter ((/=0) . snd) $ assocs a,
-    c <- xs,
-    let clues' = map (\(e:es, n) -> (es, if c==e then (n-1) else n)) clues,
-    all ((>=0) . snd) clues',
-    cs <- prob185c (l-1) clues'
-  ]
-
-prob185b l clues = map (map bit2char) (f xs0 clues)
-  where
-    f xs [] = [xs]
-    f xs ((s,r):clues) =
-      [ xs'' |
-        ms <- choices r s,
-        let xs' = zipWith (.&.) xs ms,
-        all (/=0) xs',
-        xs'' <- f xs' clues ]
-    choices 0 cs = [map nbit cs]
-    choices n [] = []
-    choices n (c:cs) =
-      [ cbit c : bs | bs <- choices (n-1) cs ] ++
-      [ nbit c : bs | bs <- choices n cs ]
-    xs0 = replicate l 0x3ff
-    cbit :: Char -> Int
-    cbit c = bit (ord c - ord '0')
-    nbit c = complement (cbit c)
--}
-
 type Mask = Int
 type Slot = Int
 type Clue' = ([Slot], Int)
@@ -284,25 +177,27 @@ number_mind clues = map (map bit2char) (search xs0 clues'')
     xs0 :: [Mask]
     xs0 = replicate l 0x3ff
 
-    search :: [Mask] -> [Clue'] -> [[Mask]]
-    search xs [] = [xs]
-    search xs ((s,r):clues) =
-      [ xs'' |
-        xs' <- choices r s xs,
-        xs'' <- search xs' clues ]
+search :: [Mask] -> [Clue'] -> [[Mask]]
+search xs [] = [xs]
+search xs ((s,r):clues) =
+  [ xs'' |
+    xs' <- choices r s xs,
+    xs'' <- search xs' clues ]
 
-    choices :: Int -> [Slot] -> [Mask] -> [[Mask]]
-    choices 0 [] [] = [[]]
-    choices 0 (c:cs) (x:xs)
-      | x == bit c = []
-      | otherwise = [ clearBit x c : bs | bs <- choices 0 cs xs ]
-    choices n [] [] = []
-    choices n (c:cs) (x:xs)
-      | x == bit c = [ bit c : bs | bs <- choices (n-1) cs xs ]
-      | testBit x c = [ bit c : bs | bs <- choices (n-1) cs xs ] ++
-                      [ clearBit x c : bs | bs <- choices n cs xs ]
-      | otherwise = [ x : bs | bs <- choices n cs xs ]
+choices :: Int -> [Slot] -> [Mask] -> [[Mask]]
+choices 0 [] [] = [[]]
+choices 0 (c:cs) (x:xs)
+  | x == bit c = []
+  | otherwise = [ clearBit x c : bs | bs <- choices 0 cs xs ]
+choices n [] [] = []
+choices n (c:cs) (x:xs)
+  | x == bit c = [ bit c : bs | bs <- choices (n-1) cs xs ]
+  | testBit x c = [ bit c : bs | bs <- choices (n-1) cs xs ] ++
+                  [ clearBit x c : bs | bs <- choices n cs xs ]
+  | otherwise = [ x : bs | bs <- choices n cs xs ]
 
 main :: IO String
 main = return $ head $ number_mind real_clues
--- 4640261571849533
+
+answer :: String
+answer = "4640261571849533"
