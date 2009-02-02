434999c4a2532eca802a75a8ff3756398184c5f9
Brian Huffman <brianh@cs.pdx.edu>
2009-02-02 15:57:33 -0800

    format comments 161

--- Euler161.hs
+++ Euler161.hs
@@ -2,8 +2,11 @@ module Euler161 where
 import Data.Array
 
 {-
-A triomino is a shape consisting of three squares joined via the edges.
-There are two basic forms:
+Problem 161
+21 September 2007
+
+A triomino is a shape consisting of three squares joined via the
+edges.  There are two basic forms:
 
  +--+--+  +--+--+--+
  |  |  |  |  |  |  |
@@ -22,12 +25,13 @@ If all possible orientations are taken into account there are six:
                                      |  |
                                      +--+
 
-Any n by m grid for which nxm is divisible by 3 can be tiled with triominoes.
-If we consider tilings that can be obtained by reflection or rotation from
-another tiling as different there are 41 ways a 2 by 9 grid can be tiled with
-triominoes:
+Any n by m grid for which nxm is divisible by 3 can be tiled with
+triominoes.  If we consider tilings that can be obtained by reflection
+or rotation from another tiling as different there are 41 ways a 2 by
+9 grid can be tiled with triominoes:
 
-In how many ways can a 9 by 12 grid be tiled in this way by triominoes?
+In how many ways can a 9 by 12 grid be tiled in this way by
+triominoes?
 -}
 
 {-
