b5ce013e01879384f13828ec1171d9ce8e3d9596
Brian Huffman <brianh@cs.pdx.edu>
2009-02-07 18:47:06 -0800

    clean up 118

--- Euler118.hs
+++ Euler118.hs
@@ -11,12 +11,13 @@ Sets of primes using all digits.
 
 24 March 2006
 
-Using all of the digits 1 through 9 and concatenating them freely to form
-decimal integers, different sets can be formed. Interestingly with the set
-{2,5,47,89,631}, all of the elements belonging to it are prime.
+Using all of the digits 1 through 9 and concatenating them freely to
+form decimal integers, different sets can be formed. Interestingly
+with the set {2,5,47,89,631}, all of the elements belonging to it are
+prime.
 
-How many distinct sets containing each of the digits one through nine exactly
-once contain only prime elements?
+How many distinct sets containing each of the digits one through nine
+exactly once contain only prime elements?
 -}
 
 -- 383 "1234567_9"
