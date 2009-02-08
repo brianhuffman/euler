a0a1090a942caf93712454f915cdef50ebe3f6a9
Brian Huffman <brianh@cs.pdx.edu>
2009-02-08 13:26:40 -0800

    clean up 186

--- Euler186.hs
+++ Euler186.hs
@@ -1,6 +1,5 @@
 module Euler186 where
 import Lagged
-import List
 import Control.Monad.ST
 import Data.Array.ST
 
@@ -10,31 +9,34 @@ Connectedness of a network.
 
 15 March 2008
 
-Here are the records from a busy telephone system with one million users:
+Here are the records from a busy telephone system with one million
+users:
+
 RecNr   Caller  Called
 1       200007  100053
 2       600183  500439
 3       600863  701497
 ...     ...     ...
 
-The telephone number of the caller and the called number in record n are
-Caller(n) = S(2n-1) and Called(n) = S(2n) where S(1), S(2), S(3), ... come
-from the "Lagged Fibonacci Generator":
+The telephone number of the caller and the called number in record n
+are Caller(n) = S(2n-1) and Called(n) = S(2n) where S(1), S(2), S(3),
+... come from the "Lagged Fibonacci Generator":
 
-For 1 <= k <= 55, S(k) = [100003 - 200003k + 300007k^3] (modulo 1000000)
-For 56 <= k, S(k) = [S(k-24) + S(k-55)] (modulo 1000000)
+For 1 <= k <= 55, S(k) = [100003 - 200003k + 300007k^3] (modulo
+1000000) For 56 <= k, S(k) = [S(k-24) + S(k-55)] (modulo 1000000)
 
-If Caller(n) = Called(n) then the user is assumed to have misdialled and the
-call fails; otherwise the call is successful.
+If Caller(n) = Called(n) then the user is assumed to have misdialled
+and the call fails; otherwise the call is successful.
 
-From the start of the records, we say that any pair of users X and Y are
-friends if X calls Y or vice-versa. Similarly, X is a friend of a friend
-of Z if X is a friend of Y and Y is a friend of Z; and so on for longer
-chains.
+From the start of the records, we say that any pair of users X and Y
+are friends if X calls Y or vice-versa. Similarly, X is a friend of a
+friend of Z if X is a friend of Y and Y is a friend of Z; and so on
+for longer chains.
 
-The Prime Minister's phone number is 524287. After how many successful calls,
-not counting misdials, will 99% of the users (including the PM) be a friend,
-or a friend of a friend etc., of the Prime Minister?
+The Prime Minister's phone number is 524287. After how many successful
+calls, not counting misdials, will 99% of the users (including the PM)
+be a friend, or a friend of a friend etc., of the Prime Minister?
+
 -}
 
 -- Disjoint sets, with size counting
@@ -71,16 +73,7 @@ unionDisjSet a m n = do
 minister :: Int
 minister = 524287
 
-{-
-prob186z calls = do
-  a <- newDisjSet (10^6)
-  sequence_ [ unionDisjSet a m n | (m,n) <- calls ]
-  (r,s) <- rootDisjSet a minister
-  return s
-
-prob186y k = runST (prob186z (take k phone_calls))
--}
-
+prob186 :: Int -> Int -> ST s Int
 prob186 z v = do
   a <- newDisjSet (10^6)
   f 1 a phone_calls
@@ -100,4 +93,6 @@ phone_calls = pairs lagged_fibonacci
 
 main :: IO String
 main = return $ show $ runST (prob186 990000 524287)
--- 2325629
+
+answer :: String
+answer = "2325629"
