83b1b676ffcf1ed5ab4a8d80290412535bdb831f
Brian Huffman <brianh@cs.pdx.edu>
2009-02-07 18:17:03 -0800

    clean up 128

--- Euler128.hs
+++ Euler128.hs
@@ -2,25 +2,17 @@ module Euler128 where
 import Primes
 import qualified SortedList as S
 
-------------------------------------------------------------------------------
--- 128. Which tiles in the hexagonal arrangement have prime differences with neighbours?
 {-
-A hexagonal tile with number 1 is surrounded by a ring of six hexagonal tiles,
-starting at "12 o'clock" and numbering the tiles 2 to 7 in an anti-clockwise
-direction.
+Problem 128
+29 September 2006
 
-New rings are added in the same fashion, with the next rings being numbered 8
-to 19, 20 to 37, 38 to 61, and so on. The diagram below shows the first three
-rings.
+A hexagonal tile with number 1 is surrounded by a ring of six
+hexagonal tiles, starting at "12 o'clock" and numbering the tiles 2 to
+7 in an anti-clockwise direction.
 
-By finding the difference between tile n and each its six neighbours we shall
-define PD(n) to be the number of those differences which are prime.
-
-For example, working clockwise around tile 8 the differences are 12, 29, 11,
-6, 1, and 13. So PD(8) = 3.
-
-In the same way, the differences around tile 17 are 1, 17, 16, 1, 11, and 10,
-hence PD(17) = 2.
+New rings are added in the same fashion, with the next rings being
+numbered 8 to 19, 20 to 37, 38 to 61, and so on. The diagram below
+shows the first three rings.
 
          20
       21    37
@@ -36,10 +28,20 @@ hence PD(17) = 2.
       28    30
          29
 
+By finding the difference between tile n and each its six neighbours
+we shall define PD(n) to be the number of those differences which are
+prime.
+
+For example, working clockwise around tile 8 the differences are 12,
+29, 11, 6, 1, and 13. So PD(8) = 3.
+
+In the same way, the differences around tile 17 are 1, 17, 16, 1, 11,
+and 10, hence PD(17) = 2.
+
 It can be shown that the maximum value of PD(n) is 3.
 
-If all of the tiles for which PD(n) = 3 are listed in ascending order to form
-a sequence, the 10th tile would be 271.
+If all of the tiles for which PD(n) = 3 are listed in ascending order
+to form a sequence, the 10th tile would be 271.
 
 Find the 2000th tile in this sequence.
 -}
@@ -108,5 +110,6 @@ prob128 m = three_prime_diffs !! (m - 1)
 
 main :: IO String
 main = return $ show $ prob128 2000
--- 14516824220
 
+answer :: String
+answer = "14516824220"
