c38d6f44b8e2fa6cf402430382e091dd4dfbe6a0
Brian Huffman <brianh@cs.pdx.edu>
2009-02-07 20:38:39 -0800

    clean up 50

--- Euler050.hs
+++ Euler050.hs
@@ -1,8 +1,22 @@
 module Euler050 where
 import Primes
 
-------------------------------------------------------------------------------
--- 50. Which prime, below one-million, can be written as the sum of the most consecutive primes?
+{-
+Problem 50
+15 August 2003
+
+The prime 41, can be written as the sum of six consecutive primes:
+41 = 2 + 3 + 5 + 7 + 11 + 13
+
+This is the longest sum of consecutive primes that adds to a prime
+below one-hundred.
+
+The longest sum of consecutive primes below one-thousand that adds to
+a prime, contains 21 terms, and is equal to 953.
+
+Which prime, below one-million, can be written as the sum of the most
+consecutive primes?
+-}
 
 -- sum of at least 20 primes, max ~ 50000
 
@@ -29,4 +43,6 @@ prob50 n = fst $ head $ prime_sum_lengths n
 
 main :: IO String
 main = return $ show $ prob50 (10^6)
--- 997651
+
+answer :: String
+answer = "997651"
