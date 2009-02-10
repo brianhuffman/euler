17cd84d39e0dfc3ae3a4fb612d5d84087761d8d8
Brian Huffman <brianh@cs.pdx.edu>
2009-02-10 06:15:26 -0800

    clean up 177

--- Euler177.hs
+++ Euler177.hs
@@ -9,9 +9,9 @@ Integer angled Quadrilaterals
 
 11 January 2008
 
-Let ABCD be a convex quadrilateral, with diagonals AC and BD. At each vertex
-the diagonal makes an angle with each of the two sides, creating eight corner
-angles.
+Let ABCD be a convex quadrilateral, with diagonals AC and BD. At each
+vertex the diagonal makes an angle with each of the two sides,
+creating eight corner angles.
 
  D------C
  |\    /|
@@ -24,16 +24,17 @@ angles.
 
 For example, at vertex A, the two angles are CAD, CAB.
 
-We call such a quadrilateral for which all eight corner angles have integer
-values when measured in degrees an "integer angled quadrilateral". An example
-of an integer angled quadrilateral is a square, where all eight corner angles
-are 45°. Another example is given by DAC = 20°, BAC = 60°, ABD = 50°,
-CBD = 30°, BCA = 40°, DCA = 30°, CDB = 80°, ADB = 50°.
+We call such a quadrilateral for which all eight corner angles have
+integer values when measured in degrees an "integer angled
+quadrilateral". An example of an integer angled quadrilateral is a
+square, where all eight corner angles are 45°. Another example is
+given by DAC = 20°, BAC = 60°, ABD = 50°, CBD = 30°, BCA = 40°, DCA =
+30°, CDB = 80°, ADB = 50°.
 
 What is the total number of non-similar integer angled quadrilaterals?
 
-Note: In your calculations you may assume that a calculated angle is integral
-if it is within a tolerance of 10^-9 of an integer value.
+Note: In your calculations you may assume that a calculated angle is
+integral if it is within a tolerance of 10^-9 of an integer value.
 -}
 
 {-
@@ -124,4 +125,8 @@ prob177 n = length (filter canonical (quadrilaterals n))
 
 main :: IO String
 main = return $ show $ prob177 180
--- 129325
+
+-- Running time seems to be O(n^4)
+
+answer :: String
+answer = "129325"
